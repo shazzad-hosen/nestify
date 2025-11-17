ff039daf36c8283dec7b72d619979b5b2967303a
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2025-11-17 21:15:34 +0600

    Fix Some Conflicts

--- cloudConfig.js
+++ cloudConfig.js
@@ -8,7 +8,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const storage = new CloudinaryStorage({
+const storage = CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: "Nestify",
